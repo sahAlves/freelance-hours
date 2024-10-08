46fbcaf577d5a2227c694648ab47a3f6ca00605b
sahAlves <sah.alves002@gmail.com>
2024-10-08 13:08:06 -0300

    wip

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,6 @@ class WelcomeController extends Controller
 
     public function __invoke(Request $request)
     {
-        return view('teste.jeremias');
+        return view('welcome');
     }
 }
