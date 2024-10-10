0a6bc5fa5a2b1fa3d812f0cf9a7ff93dee2d2295
sahAlves <sah.alves002@gmail.com>
2024-10-09 23:08:28 -0300

    atualizando posicoes

--- resources/views/components/proposals/position.blade.php
+++ resources/views/components/proposals/position.blade.php
@@ -1,4 +1,4 @@
-@props(['position'])
+@props(['position', 'status'])
 
 
 <div
@@ -8,11 +8,16 @@
         "bg-[#F1F5F9] text-[#94A3B8]" => $position == 1,
         "bg-[#FFEDD5] text-[#B45309]" => $position == 2,
         "bg-[#F1F5F9]" => $position > 2,
-
     ])>
     @if($position < 3)
         <x-ui.icons.trophy class="w-[24px] h-[24px]"/>
     @else
-        <x-ui.icons.minus class="w-[24px] h-[24px]"/>
+        @if($status == 'up')
+            <x-ui.icons.arrow-up class="w-[24px] h-[24px]"/>
+        @elseif($status == 'down')
+            <x-ui.icons.arrow-down class="w-[24px] h-[24px]"/>
+        @else
+            <x-ui.icons.minus class="w-[24px] h-[24px]"/>
+        @endif
     @endif
 </div>
